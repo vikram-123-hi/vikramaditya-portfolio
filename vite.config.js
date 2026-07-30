eb91c7589af6c1f4de36cf45247c1cd3d06745a1
Vikramaditya Swain <swainvikramaditya99@gmail.com>
2026-07-30 18:31:24 +0530

    fix: dynamic base path for cloudflare vs github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vikramaditya-portfolio/',
+  base: process.env.CF_PAGES ? '/' : '/vikramaditya-portfolio/',
 });
